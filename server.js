77135f45f60cd149768d854dd67dc571b5699dd4
alepmedeiros <alemedeiros@bsd.com.br>
2020-07-03 10:47:22 -0300

    nova atualizacao

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+  
+const hostname = '0.0.0.0';
+const port = 7000;
+
+
+http.createServer((req, res) => {
+
+res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+res.end('Olha nos aqui\n');
+
+}).listen(port, hostname, () => {
+
+console.log(`Server running at http://${hostname}:${port}/`);
+
+});
